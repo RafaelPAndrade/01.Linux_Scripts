32ed64c82646e874e87dae621a0dd91dfd59a8c5
RafaelPAndrade <rpestanadeandrade@gmail.com>
2015-12-20 21:52:20 +0000

    Create Script.sh
    Besides updating, this also alters some keys (adds Alt-Gr and Menu)

---
+++ Script.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+#Parcellite-good, vlc;
+#My first script; has 3 major functions
+echo
+echo "Hi, $USER"
+echo
+
+#Add the AltGr and menu key to the Super right key and NumLock enter, respectively
+xmodmap -e "keycode 134 = Menu"
+xmodmap -e "keycode 104 = ISO_Level3_Shift" #
+xmodmap -e "keycode 169 = Delete" #getting problems here
+
+#Updates & upgrades to installed packages, as well as remove the not needed ones
+ANSWER_YES="y" 
+
+echo "Do you want to check for updates/upgrades?(y/n)"
+read ANSWER
+echo
+if [ "$ANSWER" = "$ANSWER_YES" ] ; then
+	sudo apt-get update
+	sudo apt-get upgrade
+	sudo apt-get autoremove
+
+else
+	echo "Not updated"
+fi
+
+
+#The tradicional echo example, indicates if everything is OKs 
+echo
+echo "Bom dia"
+echo "Pode agora continuar"
+echo
